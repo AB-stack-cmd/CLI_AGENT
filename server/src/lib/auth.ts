b36caa4b89448733413c95bf0655c0c4f3bad705
ABxCODE <abirsarkar123455@gmail.com>
2026-03-08 01:34:52 +0530

    auth update

--- server/src/lib/auth.ts
+++ server/src/lib/auth.ts
@@ -1,15 +1,14 @@
 import { betterAuth } from "better-auth";
-<<<<<<< HEAD
-import { prismaAdapter } from "better-auth/adapters/prisma";
-// If your Prisma file is located elsewhere, you can change the path
+import { prisma } from "./prisma";
 
 
-export const auth = betterAuth({
-  //...
-  database: prismaAdapter()
-=======
+export const auth = betterAuth(
+  {
+    database:prisma,
+    emailAndPassword : {enabled:true},
 
-export const auth = betterAuth({
-  //...
->>>>>>> 352929ec5fa5ff03440af4f514762ac6d589fedf
-});
+    session:{
+      expiresIn : 60 * 60* 24 * 7
+    }
+  }
+);
--- server/src/lib/prisma.ts
+++ server/src/lib/prisma.ts
@@ -1,5 +1,7 @@
 import { PrismaClient } from "@prisma/client"
 
+
+//connect to neon db key
 export const prisma = new PrismaClient({
   datasourceUrl: process.env.DATABASE_URL!
 })
