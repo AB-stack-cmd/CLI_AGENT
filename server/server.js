df2e25e3b06111f6a2797b1d2cf52f1aa1c798dc
ABxCODE <abirsarkar123455@gmail.com>
2026-03-09 22:46:47 +0530

    update auth

--- server/server.js
+++ server/server.js
@@ -1,11 +1,17 @@
 import express from "express"
 import CORS from "cors"
+import { auth } from "./src/lib/auth"
+import { toNodeHandler } from "better-auth/node";
 
 const app = express()
 
 app.use(CORS())
 
 const port = 3002;
+
+app.all("/api/auth/*", toNodeHandler(auth)); // connect every routh to auth instance
+
+app.use(express.json())
 
 app.get("/", (req, res) => {
     res.send("server Running,..")
